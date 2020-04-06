9e28381fa89a051b49cbf0e584ac47d0b6b9f693
ntegan <ntegan1@gmail.com>
2020-04-05 21:48:54 -0500

    .

--- 01/p1.1/main.s
+++ 01/p1.1/main.s
@@ -1,14 +1,31 @@
-# Read only data
 .section .rodata
+# Read only data
+
+usg_str:          .asciz    "Usage: %s <in_file>\n"
+no_file:          .asciz    "Couldn't open file '%s'"
+r_m:              .asciz    "r"
+out:              .asciz    "%d\n"
 
 
-# "Uninit'd Data" global + static
+
 .bss
+# "Uninit'd Data" global + static
+## .asciz == adds \0 to str for you
 
-# fixed size
+# .equiv checks if symbol already defined
+.equiv BUF_SZ,     4096
+
+# .space == .skip.
+##  ", 0" is optional fill value
+buf:              .space    BUF_SZ, 0
+
+
 .text
+# fixed size
 
+
+
+.globl main
 # make symbol visible to 'ld'
 #     .global or .globl
-.globl main
 
